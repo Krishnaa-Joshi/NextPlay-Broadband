fef12d3bbefed9205acbe5082f2d330fcc7eded4
Krishna Joshi <krishnajos01@gmail.com>
2026-02-12 13:25:33 +0530

    vercel setup

--- assets/js/main.js
+++ assets/js/main.js
@@ -207,7 +207,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/get-connection", {
+      const res = await fetch("https://next-play-broadband.vercel.app/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ dotenv.config();
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://next-play-broadband.vercel.app"
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
